bd1ac18c33335231e8f02882bad0353c05dbb1ca
Victor Ramirez <vicdeveloper83@gmail.com>
2025-04-24 20:21:44 +0100

    db created

--- src/App.jsx
+++ src/App.jsx
@@ -102,39 +102,6 @@ function FriendList({ friends, onSelection, selectedFriend }) {
 	);
 }
 
-// function Friend({ friend, onSelection, selectedFriend, onDelete }) {
-// 	const isSelected = selectedFriend?.id === friend.id;
-
-// 	return (
-// 		<li className={isSelected ? 'selected' : ''}>
-// 			<Avatar name={friend.name} background={friend.background} />
-// 			<h3>{friend.name}</h3>
-// 			{friend.balance < 0 && (
-// 				<p className='red'>
-// 					You owe {friend.name} £{Math.abs(friend.balance)}
-// 				</p>
-// 			)}
-// 			{friend.balance > 0 && (
-// 				<p className='green'>
-// 					{friend.name} owes you £{Math.abs(friend.balance)}
-// 				</p>
-// 			)}
-// 			{friend.balance === 0 && <p>You and {friend.name} are even</p>}
-// 			<div className='friend-actions'>
-// 				<Button text='Select' onClick={() => onSelection(friend)} />
-// 				<Button
-// 					text='❌'
-// 					onClick={(e) => {
-// 						e.stopPropagation();
-// 						onDelete(friend.id);
-// 					}}
-// 					className='delete-btn'
-// 				/>
-// 			</div>
-// 		</li>
-// 	);
-// }
-
 function FormAddFriend({ onSubmit, onAddFriend }) {
 	const [name, setName] = useState('');
 
