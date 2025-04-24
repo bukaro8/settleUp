312a014a639173b51eb43833c6992e4020b3af5b
Victor Ramirez <vicdeveloper83@gmail.com>
2025-04-24 21:03:24 +0100

    header created

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+	return (
+		<header className='header'>
+			<h1>SettleUp</h1>
+			<p className='tagline'>Split bills and expenses with friends</p>
+		</header>
+	);
+}
