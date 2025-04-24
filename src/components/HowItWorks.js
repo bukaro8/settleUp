126779c9320d19bef60b3870b16c95eb644aa3a7
Victor Ramirez <vicdeveloper83@gmail.com>
2025-04-24 21:03:36 +0100

    how it works created

---
+++ src/components/HowItWorks.js
@@ -0,0 +1,21 @@
+export default function HowItWorks() {
+	return (
+		<div className='how-it-works'>
+			<h2>How It Works</h2>
+			<div className='steps'>
+				<div className='step'>
+					<span>1</span>
+					<p>Add friends you want to split expenses with</p>
+				</div>
+				<div className='step'>
+					<span>2</span>
+					<p>Select a friend and enter a bill amount</p>
+				</div>
+				<div className='step'>
+					<span>3</span>
+					<p>Specify who paid and settle up!</p>
+				</div>
+			</div>
+		</div>
+	);
+}
