9be695c2429783df203d4a3375ead8be52c59b00
Victor Ramirez <vicdeveloper83@gmail.com>
2025-04-24 21:04:46 +0100

    footer created

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+	return (
+		<footer className='footer'>
+			<p>© {new Date().getFullYear()} SettleUp App</p>
+			<p className='credit'>Created with ❤️ for Victor Ramirez</p>
+		</footer>
+	);
+}
